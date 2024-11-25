14a704e5fef1cae17da4fbea620908a96318a075
cookielover2008 <mashaischenko6666@gmail.com>
2024-11-25 21:36:59 +0300

    Update zadacha1pz5.py

--- PZ5/zadacha1pz5.py
+++ PZ5/zadacha1pz5.py
@@ -1,17 +1,18 @@
-def summa_cifr(n):
+def summa_cyfr(n):
     return sum(int(digit) for digit in str(n))
 def shagi_do_zero(n):
     shagi = 0
     while n > 0:
-        n -= summa_cifr(n)
+        n -= summa_cyfr(n)
         shagi += 1
     return shagi
 
 n = input('Введите число N: ')
-while type(n) != int:
+while type(a) != int:                   # обработка исключений для N
     try:
-        n = int(n)
+        a = int(a)
     except ValueError:
-        print('Неправильно веели!')
-rezultat = shagi_do_zero(n)
+        print("Неправильно ввели!")
+        a = input("Введите A заново: ")
+rezultat = shagi_do_zero(n)           
 print('Количество шагов до нуля: ', rezultat)
