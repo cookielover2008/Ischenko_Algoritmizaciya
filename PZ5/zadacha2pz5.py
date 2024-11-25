34ea97bbea26bc50d203c9c914c46a377cccc1e5
cookielover2008 <mashaischenko6666@gmail.com>
2024-11-25 21:59:41 +0300

    Add files via upload

--- PZ5/zadacha2pz5.py
+++ PZ5/zadacha2pz5.py
@@ -11,26 +11,34 @@ while type(a) != int:
         a = int(a)
     except ValueError:
         print('Неправильно ввели!')
+        a = input("Введи A заново: ")
 b = input('Введи число B: ')
 while type(b) != int:
     try:
         b = int(b)
     except ValueError:
         print('Неправильно ввели!')
+        b = input("Введи B заново: ")
 c = input('Введи число C: ')
 while type(c) != int:
     try:
         c = int(c)
     except ValueError:
         print('Неправильно ввели!')
+        c = input("Введи C заново: ")
 d = input('Введи число D: ')
 while type(d) != int:
     try:
         d = int(d)
     except ValueError:
         print('Неправильно ввели!')
-min_value, max_value = Minmax(a, b)
-min_value, max_value = Minmax(min_value, c)
-min_value, max_value = Minmax(min_value, d)
-print('Максимальное число: ', max_value)
-print('Минимальное число: ', min_value)
+        d = input("Введи D заново: ")
+
+
+min1, max1 = Minmax(a, b)  # Минимум и максимум для A и B
+min2, max2 = Minmax(c, d)  # Минимум и максимум для C и D
+samoe_minimalnoe, zub_dayu = Minmax(min1, min2)  # Минимум из двух минимальных значений
+zub_dayu, samoe_maximalnoe = Minmax(max1, max2)  # Максимум из двух максимальных значений
+
+print('Минимальное число: ', samoe_minimalnoe)
+print('Максимальное число: ', samoe_maximalnoe)
