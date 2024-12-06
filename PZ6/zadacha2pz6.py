644be869dc2d5032b43803c026d1ae4380b262d6
cookielover2008 <mashaischenko6666@gmail.com>
2024-12-06 13:13:55 +0300

    Update zadacha2pz6.py

--- PZ6/zadacha2pz6.py
+++ PZ6/zadacha2pz6.py
@@ -1,14 +1,11 @@
-#Дан список размера N. Найти количество его промежутков монотонности
-#(то есть участков, на которых его элементы возрастают или убывают).
 def schitaem_ciferki(spisok):
     if len(spisok) < 2:
-        print('Невозможно выполнить операцию! Введи список снова')
-        return []
-    
+        print('Невозможно выполнить операцию! Введите список снова')
+        return None
 
-    schet = 0           # Cчётчик промежутков монотонности    
-    povishenie = None       # Задаем начальное состояние
-    
+    schet = 0  # Счетчик промежутков монотонности
+    povishenie = None  # Задаем начальное состояние
+
     for i in range(1, len(spisok)):
         if spisok[i] > spisok[i - 1]:  # Если текущий элемент больше предыдущего
             if povishenie is False:  # Если до этого был убывающий участок
@@ -17,14 +14,17 @@ def schitaem_ciferki(spisok):
         elif spisok[i] < spisok[i - 1]:  # Если текущий элемент меньше предыдущего
             if povishenie is True:  # Если до этого был возрастающий участок
                 schet += 1
-            povishenie = False  # Устанавливаем состояние как убывающее   
-    if povishenie is not None:      # Если последний участок был монотонным, то добавляем его
+            povishenie = False  # Устанавливаем состояние как убывающее
+
+    # Учитываем последний участок, если он существует и не равен None
+    if povishenie is not None:
         schet += 1
 
     return schet
 
 # Пример использования
 spisok = [3, 1, 2, 4, 3, 5, 6, 2]  # Примерный список
-print('Ваш список: ', spisok)
+print('Ваш список:', spisok)
 result = schitaem_ciferki(spisok)
-print("Количество промежутков монотонности:", result)
+if result is not None:
+    print("Количество промежутков монотонности:", result)
