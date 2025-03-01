1cf1e38682cdd573f012071663ee34b17e7a1eda
cookielover2008 <mashaischenko6666@gmail.com>
2025-03-01 12:46:16 +0300

    Add files via upload

--- PZ9/pz10.py
+++ PZ9/pz10.py
@@ -4,22 +4,25 @@
 #   1. в каких турагенствах можно приобрести туры в Японию.
 #   2. в каких турагенствах нельзя приобрести туры в ЮАР.
 #   3. полный список всех туров.
-tours = {
-"Вояж": ['Мексика', 'Канада', 'Израиль', 'Италия', 'США'],
-"РейнаТур": ['Англия', 'Япония', 'Канада', 'ЮАР'],
-"Радуга": ['США', 'Испания', 'Швеция', 'Австралия']
-}
+
+voyage = {'Мексика', 'Канада', 'Израиль', 'Италия', 'США'}
+reynatour = {'Англия', 'Япония', 'Канада', 'ЮАР'}
+rainbow = {'США', 'Испания', 'Швеция', 'Австралия'}
+
 est_japan = []
 net_yuar = []
+def est_li (a, b, c):
+    if 'Япония' in voyage or reynatour or rainbow:
+        est_japan.append(voyage or reynatour or rainbow)
+    if 'ЮАР' not in voyage or reynatour or rainbow:
+        net_yuar.append(voyage or reynatour or rainbow)
+    return est_japan
+    return net_yuar
 
-for i, f in tours.items():
-    if 'Япония' in f:
-        est_japan.append(i)
-    if 'ЮАР' not in f:
-        net_yuar.append(i)
+
+est_li(voyage, reynatour, rainbow)
+
 
 print('Турагенства с туром в Японию: ', est_japan)
 print('Турагенства без туров в ЮАР: ', net_yuar)
-print('Все туры: ', set(f))
-
-
+print('Все туры: ', voyage|reynatour|rainbow)
