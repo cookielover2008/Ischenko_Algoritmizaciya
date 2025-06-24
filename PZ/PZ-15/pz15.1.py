99ca966cd8f93a7332e9eec8642f015f883d259a
cookielover2008 <mashaischenko6666@gmail.com>
2025-06-24 23:30:38 +0300

    Add files via upload

--- PZ/PZ-15/pz15.1.py
+++ PZ/PZ-15/pz15.1.py
@@ -1,19 +1,17 @@
 # Приложение ПРОМЫШЛЕННОСТЬ для автоматизированного учета
-# информации о промышленных предприятиях республики. БД содержит таблицу
-# Предприятия, имеющую следующую структуру записи: Код предприятия, Наименование
-# предприятия, Физический адрес, Филиалы (количество филиалов), Общая числ. персонала,
-# Общая стоим. оборудования, Объем выпускаемой продукции, Дата регистрации.
+# информации о промышленных предприятиях республики.
 
 import sqlite3
-
-conn = sqlite3.connect("Enterprises.db")
 
 def execute_sql_from_file(conn, sql_file):
     with open(sql_file, 'r', encoding='utf-8') as file:
         sql_script = file.read()
         cursor = conn.cursor()
-        cursor.execute(sql_script)
+        cursor.executescript(sql_script)  # Исправлено: выполняем весь скрипт
         conn.commit()
 
-execute_sql_from_file(conn, 'code.sql')
-conn.close()
+if __name__ == "__main__":
+    conn = sqlite3.connect("Enterprises.db")
+    execute_sql_from_file(conn, "D:\PzManakova\pz15\code.sql")
+    conn.close()
+    print("База данных и таблица успешно созданы.")
