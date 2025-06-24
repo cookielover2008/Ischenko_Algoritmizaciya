8a97a8072ceae93c600dec54dda233c3e32c938b
cookielover2008 <mashaischenko6666@gmail.com>
2025-06-24 22:55:48 +0300

    Add files via upload

--- PZ/PZ-14/pz14.1.py
+++ PZ/PZ-14/pz14.1.py
@@ -1,8 +1,15 @@
-# Из исходного текстового файла (pazzl.html) выбратьвсе html-коды изображений.
-# Посчитать их количество.
+# В исходном текстовом файле(Dostoevsky.txt) найти все фамилии с инициалами
+# (например, А. Ф. Куманиной и т.п.).
 
 import re
-text = open("Dostoevsky.txt", "r+", encoding="utf-8").read()
 
-ex = re.compile(r'\b([А-ЯЁ][.])\s([А-ЯЁ][.])\s([А-ЯЁа-яё]+)\b')
-print(ex.findall(text))
+with open('D:\PzManakova\pz14\Dostoevsky.txt', 'r', encoding='utf-8') as file:      # Открываем файл для чтения в кодировке utf-8
+    text = file.read()
+
+pattern = r'\b(?:[А-Я]\.\s){1,2}[А-Я][а-яё]+\b'     # Регулярное выражение для поиска инициалов + фамилии
+
+matches = re.findall(pattern, text) # Ищем все совпадения
+
+unique_matches = set(matches)       # Выводим фамилии с инициалами
+for match in unique_matches:
+    print(match)
